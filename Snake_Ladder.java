242ceacc2f26cd78f5478acbf9ab9ce166abdcbc
sagar <sagarbehera156@gmail.com>
2022-11-03 15:30:52 +0530

    Diplaying Welcome to Snake And Ladder Computation Program

---
+++ Snake_Ladder.java
@@ -0,0 +1,10 @@
+package day4;
+
+public class Snake_Ladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Snake And Ladder Computation Program");
+	}
+
+}
