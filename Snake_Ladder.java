7d8a4218294f4dfc391da743ae1e68e4f9c8a1d1
sagar <sagarbehera156@gmail.com>
2022-11-03 15:36:40 +0530

    Add: Player Position

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -1,10 +1,13 @@
 package day4;
 
 public class Snake_Ladder {
+	static int player = 0;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Snake And Ladder Computation Program");
+		System.out.println(" Player is at position :" + player);
+
 	}
 
 }
